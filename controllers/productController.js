f61d6bda18947e6502c66cd1c341e2ed7408b87b
Elfin Sanjaya <elfinsanjaya1207@gmail.com>
2019-08-20 18:59:22 +0700

    Update and Delete Product

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,6 +9,14 @@
 const { Product } = require('../models')
 const Op = require("sequelize").Op
 
+/**
+ * Validation 
+ *
+ * @param object req
+ * @param object res
+ * @return array
+ */
+
 async function validate(req) {
   let {
     code,
@@ -90,6 +98,13 @@ async function validate(req) {
   return errors
 }
 
+/**
+ * Verify data product
+ * 
+ * @param object req
+ * @param object res 
+ * @return json
+ */
 exports.actionCreate = async (req, res) => {
   let {
     code,
@@ -123,14 +138,140 @@ exports.actionCreate = async (req, res) => {
   return res.status(200).json(product)
 }
 
+/**
+ * Read all product
+ * 
+ * @param object req
+ * @param object res 
+ * @return json
+ */
 exports.actionRealAll = async function (req, res) {
-  const products = await Product.findAll()
+  try {
+    const products = await Product.findAll()
+    return res.status(200).json({
+      message: "Success Read All Products",
+      products
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
 
-  return res.status(200).json({
-    message: "Success Read All Products",
-    products
-  })
+/**
+ * Get single product
+ *
+ * @param object req
+ * @param object res
+ * @return json
+ */
+
+exports.actionReadSingleProduct = async function (req, res) {
+  const { id } = req.params
+
+  let error = await validateReadSingleProduct(req)
+  if (error.length > 0) return res.status(422).json({ error })
+  try {
+    const product = await Product.findOne({
+      where: { id: { [Op.eq]: id } }
+    })
+
+    return res.status(201).json({
+      message: "Success Read Single Product",
+      product
+    })
+  } catch (err) {
+    console.log(err)
+  }
 
 }
 
+async function validateReadSingleProduct(req) {
+  const { id } = req.params
+  let errors = []
+  if (id) {
+    try {
+      const product = await Product.findOne({
+        where: {
+          id: { [Op.eq]: id },
+        }
+      })
+      if (product === null || product === "") {
+        errors.push({
+          field: 'id',
+          message: 'Id Not Found',
+        })
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  return errors
+}
 
+exports.actionUpdate = async function (req, res) {
+  const id = req.params.id;
+  let {
+    code,
+    name,
+    material,
+    ukuran,
+    warna,
+    harga,
+    stok,
+    like,
+    deskripsi,
+    image
+  } = req.body
+
+  let error = await validate(req)
+  if (error.length > 0) return res.status(422).json({ error })
+
+  try {
+    const product = await Product.findOne({
+      where: {
+        id: { [Op.eq]: id }
+      }
+    })
+
+    if (product) {
+      product.code = code
+      product.name = name
+      product.material = material
+      product.ukuran = ukuran
+      product.warna = warna
+      product.harga = harga
+      product.stok = stok
+      product.like = like
+      product.deskripsi = deskripsi
+      product.image = image
+      await product.save();
+    }
+    res.statusMessage = 'Data has been changed';
+    return res.status(201).json({
+      message: "Success Update Product",
+      product
+    })
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+exports.actionDelete = async function (req, res) {
+  const { id } = req.params;
+
+  let error = await validateReadSingleProduct(req)
+  if (error.length > 0) return res.status(422).json({ error })
+
+  Product.findOne({ where: { id: { [Op.eq]: id } } })
+    .then((product) => {
+      return product.destroy();
+    })
+    .then((product) => {
+      res.status(200).json({ message: "Success Delete", product });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Internal server error" });
+    });
+
+};
--- routes/product.js
+++ routes/product.js
@@ -2,12 +2,18 @@ let express = require("express")
 const auth = require('../middlewares/auth');
 const {
   actionCreate,
-  actionRealAll
+  actionRealAll,
+  actionReadSingleProduct,
+  actionUpdate,
+  actionDelete
 } = require("../controllers/productController")
 let router = express.Router()
 
 router.use("/product", auth)
 router.post("/product", actionCreate)
 router.get("/product", actionRealAll)
+router.get("/product/detail/:id", actionReadSingleProduct)
+router.put("/product/:id", actionUpdate)
+router.delete("/product/:id", actionDelete)
 
 module.exports = router
