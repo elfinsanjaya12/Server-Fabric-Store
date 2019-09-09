167bb38a9ef7c17eccc04a5ab4b06f9da521d44d
Elfin Sanjaya <elfinsanjaya1207@gmail.com>
2019-09-09 13:48:38 +0700

    Read All Customers

--- routes/customer.js
+++ routes/customer.js
@@ -3,10 +3,13 @@ let router = express.Router()
 
 const {
   actionRegisterMobile,
-  actionLogin
+  actionLogin,
+  actionRead
 } = require("../controllers/customerController")
 
 router.post("/customer/register", actionRegisterMobile)
 router.post("/customer/signin", actionLogin)
+
+router.get("/customer", actionRead)
 
 module.exports = router
