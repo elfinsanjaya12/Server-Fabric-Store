78f5d274790126bd54419bc326c1c31ee6f86666
Elfin Sanjaya <elfinsanjaya1207@gmail.com>
2019-10-21 10:59:30 +0700

    add seeder

--- seeders/20190827023401-seeder-address.js
+++ seeders/20190827023401-seeder-address.js
@@ -5,28 +5,28 @@ module.exports = {
     return queryInterface.bulkInsert('Addresses', [
       {
         CustomerId: 1,
-        // ProvinceId: 11,
-        // CitiesId: 1101,
+        ProvinceId: 1,
+        CitiesId: 17,
         // DistrictId: 1101010,
-        mainAddress: "Jln. Abdul Kadir Gg. Pipit No.225",
+        mainAddress: "",
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         CustomerId: 2,
-        // ProvinceId: 11,
-        // CitiesId: 1101,
+        ProvinceId: 1,
+        CitiesId: 17,
         // DistrictId: 1101010,
-        mainAddress: "Bandar Lampung",
+        mainAddress: "",
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         CustomerId: 3,
-        // ProvinceId: 11,
-        // CitiesId: 1101,
+        ProvinceId: 1,
+        CitiesId: 17,
         // DistrictId: 1101010,
-        mainAddress: "Bandar Lampung",
+        mainAddress: "",
         createdAt: new Date(),
         updatedAt: new Date()
       }
